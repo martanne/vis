172bee98411bdc6be58428612ce8c826a1dbcd9a
Marc André Tanner <mat@brain-dump.org>
2015-11-08 14:39:01 +0100

    Update year numbers in Copyright clause

--- text-motions.c
+++ text-motions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Marc André Tanner <mat at brain-dump.org>
+ * Copyright (c) 2014-2015 Marc André Tanner <mat at brain-dump.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
