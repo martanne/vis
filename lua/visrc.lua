3e47a926e7687d52ac2df06d5b664afd4f644971
Josh Wainwright <josh.wainwright@ldra.com>
2016-04-21 10:37:30 +0100

    Use dofile() rather than require() to run lua test code

--- lua/visrc.lua
+++ lua/visrc.lua
@@ -1,11 +1,11 @@
-require("utils")
+dofile("utils.lua")
 
 vis.events = {}
 vis.events.win_open = function(win)
 	-- test.in file passed to vis
 	in_file = win.file.name
 	-- use the corresponding test.lua file
-	lua_file = string.gsub(in_file, '%.in$', '')
-	require(lua_file)
+	lua_file = string.gsub(in_file, '%.in$', '.lua')
+	dofile(lua_file)
 	vis:command('q!')
 end
