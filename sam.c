d3798ba58d3f8ebef9a024c8d538d7bebb0b6931
Marc André Tanner <mat@brain-dump.org>
2016-09-25 10:39:03 +0200

    sam: change license header
    We use an adapted variant of sam's structural regular expression
    based command language. The initial implementation was partially
    based upon the following functions from sam / acme:
    
     * parse.h / edit.h (struct definitions)
     * cmd.c / edit.c (functions parsecmd, simpleaddr, compoundaddr)
     * xec.c / ecmd.c (cmdexec)
     * address.c / addr.c (address)
    
    It turns out the relevant code can be traced back to the initial
    X11 port of sam which is distributed under an ISC-like license
    instead of the Lucent Public License Version 1.02 used for Plan 9,
    plan9port and 9base.
    
     http://www.netlib.org/research/
     http://www.netlib.org/research/sam.shar
    
    Hence we switch to the simpler license variant.
    
    Close #238

--- sam.c
+++ sam.c
@@ -1,9 +1,20 @@
 /*
- * Heavily inspired (and partially based upon) Rob Pike's sam text editor
- * for Plan 9. Licensed under the Lucent Public License Version 1.02.
+ * Heavily inspired (and partially based upon) the X11 version of
+ * Rob Pike's sam text editor originally written for Plan 9.
  *
- *  Copyright © 2000-2009 Lucent Technologies
  *  Copyright © 2016 Marc André Tanner <mat at brain-dump.org>
+ *  Copyright © 1998 by Lucent Technologies
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose without fee is hereby granted, provided that this entire notice
+ * is included in all copies of any software which is or includes a copy
+ * or modification of this software and in all copies of the supporting
+ * documentation for such software.
+ *
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. IN PARTICULAR, NEITHER THE AUTHORS NOR LUCENT TECHNOLOGIES MAKE ANY
+ * REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+ * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
  */
 #include <string.h>
 #include <strings.h>
