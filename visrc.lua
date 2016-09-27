34acb0ba0589b94f36596fbef2c7b59f0997456f
Marc André Tanner <mat@brain-dump.org>
2016-09-27 22:46:58 +0200

    visrc: move global settings to start handler
    There is no need to set global settings for each window.

--- visrc.lua
+++ visrc.lua
@@ -1,11 +1,15 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
 
+vis.events.start = function()
+	-- Your global configuration options e.g.
+	-- vis:command('map! normal j gj')
+end
+
 vis.events.win_open = function(win)
 	-- enable syntax highlighting for known file types
 	vis.filetype_detect(win)
 
-	-- Your local configuration options e.g.
+	-- Your per window configuration options e.g.
 	-- vis:command('set number')
-	-- vis:command('map! normal j gj')
 end
