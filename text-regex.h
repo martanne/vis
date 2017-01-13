4597e40a10ac80362b349a3abb5027349443d726
Marc André Tanner <mat@brain-dump.org>
2017-01-13 12:23:09 +0100

    text-regex: slightly clean up header
    We generally omit parameter names in function prototypes when their
    meaning is clear.

--- text-regex.h
+++ text-regex.h
@@ -8,8 +8,8 @@ typedef struct Regex Regex;
 typedef Filerange RegexMatch;
 
 Regex *text_regex_new(void);
-int text_regex_compile(Regex *r, const char *regex, int cflags);
-void text_regex_free(Regex *r);
+int text_regex_compile(Regex*, const char *pattern, int cflags);
+void text_regex_free(Regex*);
 int text_regex_match(Regex*, const char *data, int eflags);
 int text_search_range_forward(Text*, size_t pos, size_t len, Regex *r, size_t nmatch, RegexMatch pmatch[], int eflags);
 int text_search_range_backward(Text*, size_t pos, size_t len, Regex *r, size_t nmatch, RegexMatch pmatch[], int eflags);
