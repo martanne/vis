a445d8f1ad028f5ffcf17cf394574267e55c6e3c
Marc André Tanner <mat@brain-dump.org>
2017-02-20 12:06:28 +0100

    util: add overflow safe unsigned addition function

--- util.h
+++ util.h
@@ -1,6 +1,9 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #define LENGTH(x)  ((int)(sizeof (x) / sizeof *(x)))
 #define MIN(a, b)  ((a) > (b) ? (b) : (a))
 #define MAX(a, b)  ((a) < (b) ? (b) : (a))
@@ -8,5 +11,16 @@
 /* is c the start of a utf8 sequence? */
 #define ISUTF8(c)   (((c)&0xC0)!=0x80)
 #define ISASCII(ch) ((unsigned char)ch < 0x80)
+
+#if GCC_VERSION>=5004000 || CLANG_VERSION>=4000000
+#define addu __builtin_add_overflow
+#else
+static inline bool addu(size_t a, size_t b, size_t *c) {
+	if (SIZE_MAX - a < b)
+		return false;
+	*c = a + b;
+	return true;
+}
+#endif
 
 #endif
