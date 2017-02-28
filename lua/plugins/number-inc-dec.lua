e8798d8bbb1ce6b384194e6211e60d5ea47ba679
Denis Warsow <stdedjub@googlemail.com>
2017-02-28 16:17:26 +0000

    Modify increment/decrement number to work with similiar strings i.e
    HEX0xdeadbeefHEX
    OCT0230OCT
    foo218
    12359.1237

--- lua/plugins/number-inc-dec.lua
+++ lua/plugins/number-inc-dec.lua
@@ -12,10 +12,21 @@ local change = function(delta)
 
 	for cursor in win:cursors_iterator() do
 		local pos = cursor.pos
-		local word = file:text_object_word(pos);
-		local s, e = word.start, word.finish;
+
+		local word_range = file:text_object_word(pos);
+		local word_start = word_range.start
+		local word = file:content(word_range.start,word_range.finish)
+		local Cp = lpeg.Cp()
+		-- create Pattern for hex,oct,int
+		local int_pattern = lpeg.S('+-')^-1 * lexer.dec_num
+		local pattern = lpeg.P{ Cp * (lexer.hex_num + lexer.oct_num + int_pattern ) * Cp +1 * lpeg.V(1) }
+		local s, e = pattern:match(word)
+
 		if s then
-			local data = file:content(s, e - s)
+			local data = string.sub(word,s,e-1)
+			-- align start and end for fileindex
+			s = word_start+s-1
+			e = word_start+e
 			if data and #data > 0 then
 				local base, format, padding = 10, 'd', 0
 				if lexer.oct_num:match(data) then
@@ -31,7 +42,8 @@ local change = function(delta)
 				if number then
 					number = number + delta * count
 					if base ~= 10 and number < 0 then
-						-- string.format does not handle negative hex/oct values
+						-- http://www.lua.org/manual/5.1/manual.html#pdf-tonumber
+						-- tonumber does not handle negative hex/oct values
 						-- should we wrap around?
 						number = 0
 					end
@@ -39,11 +51,12 @@ local change = function(delta)
 					if base == 8 and string.sub(number, 0, 1) ~= "0" then
 						number = '0' .. number
 					end
-					file:delete(s, e - s)
+					-- need to increment s for length
+					file:delete(s, e-(s+1))
 					file:insert(s, number)
 					cursor.pos = s
 				else
-					vis:info("Not a number")
+	--				vis:info("Not a number")
 				end
 			end
 		end
