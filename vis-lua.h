d44ae5817c9210c89c584aa837a400e13f423804
Marc André Tanner <mat@brain-dump.org>
2017-03-24 10:51:49 +0100

    vis-lua: removed unused function declaration

--- vis-lua.h
+++ vis-lua.h
@@ -36,7 +36,6 @@ void vis_lua_file_close(Vis*, File*);
 void vis_lua_win_open(Vis*, Win*);
 void vis_lua_win_close(Vis*, Win*);
 void vis_lua_win_highlight(Vis*, Win*);
-bool vis_lua_win_syntax(Vis*, Win*, const char *syntax);
 void vis_lua_win_status(Vis*, Win*);
 
 #endif
