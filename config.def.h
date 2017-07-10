martanne/vis

commit 71eab6d5d72145f17ab3d4c87945ac12176ae8e9

text: remove text_history_get function
As currently implemented this does not properly integrate with
multiple cursor support. The functionality should be provided
in a layer higher up.

The jumplist and changelist need to be redesigned, for now they
are broken.

vis-motions.c
@@ -187,7 +187,7 @@ static size_t window_changelist_next(Vis *vis, Win *win, size_t pos) {
 		cl->index = 0;
 	else if (cl->index > 0 && pos == cl->pos)
 		cl->index--;
-	size_t newpos = text_history_get(txt, cl->index);
+	size_t newpos = pos;
 	if (newpos == EPOS)
 		cl->index++;
 	else
@@ -204,7 +204,7 @@ static size_t window_changelist_prev(Vis *vis, Win *win, size_t pos) {
 		cl->index = 0;
 	else if (pos == cl->pos)
 		win->changelist.index++;
-	size_t newpos = text_history_get(txt, cl->index);
+	size_t newpos = pos;
 	if (newpos == EPOS)
 		cl->index--;
 	else

text.c
@@ -1675,15 +1675,3 @@ size_t text_mark_get(Text *txt, Mark mark) {
 
 	return EPOS;
 }
-
-size_t text_history_get(Text *txt, size_t index) {
-	for (Revision *rev = txt->current_revision ? txt->current_revision : txt->history; rev; rev = rev->prev) {
-		if (index-- == 0) {
-			Change *c = rev->change;
-			while (c && c->next)
-				c = c->next;
-			return c ? c->pos : EPOS;
-		}
-	}
-	return EPOS;
-}

text.h
@@ -357,7 +357,4 @@ ssize_t text_write_range(Text*, Filerange*, int fd);
 bool text_mmaped(Text*, const char *ptr);
 /** @} */
 
-/* get position of change denoted by index, where 0 indicates the most recent */
-size_t text_history_get(Text*, size_t index);
-
 #endif

commit c37fd32f2023a5d52b38b769e3d48d1f435fe2fd

vis: remove change list
This was completely broken since 71eab6d5d72145f17ab3d4c87945ac12176ae8e9
and even before never really worked as one would expect.

If anything it should be implemented like the jump list using marks.

config.def.h
@@ -241,8 +241,6 @@ static const KeyBinding bindings_normal[] = {
 	{ "<F1>",               ALIAS(":help<Enter>")                       },
 	{ "ga",                 ACTION(UNICODE_INFO)                        },
 	{ "g8",                 ACTION(UTF8_INFO)                           },
-	{ "g,",                 ACTION(CHANGELIST_NEXT)                     },
-	{ "g;",                 ACTION(CHANGELIST_PREV)                     },
 	{ "g-",                 ACTION(EARLIER)                             },
 	{ "g+",                 ACTION(LATER)                               },
 	{ "gn",                 ALIAS("vgn")                                },

main.c
@@ -215,8 +215,6 @@ enum {
 	VIS_ACTION_JUMPLIST_PREV,
 	VIS_ACTION_JUMPLIST_NEXT,
 	VIS_ACTION_JUMPLIST_SAVE,
-	VIS_ACTION_CHANGELIST_PREV,
-	VIS_ACTION_CHANGELIST_NEXT,
 	VIS_ACTION_UNDO,
 	VIS_ACTION_REDO,
 	VIS_ACTION_EARLIER,
@@ -668,16 +666,6 @@ static const KeyAction vis_action[] = {
 		"vis-jumplist-save",
 		VIS_HELP("Save current selections in jump list")
 		jumplist, { .i = 0 }
-	},
-	[VIS_ACTION_CHANGELIST_PREV] = {
-		"vis-changelist-prev",
-		VIS_HELP("Go to older cursor position in change list")
-		movement, { .i = VIS_MOVE_CHANGELIST_PREV }
-	},
-	[VIS_ACTION_CHANGELIST_NEXT] = {
-		"vis-changelist-next",
-		VIS_HELP("Go to newer cursor position in change list")
-		movement, { .i = VIS_MOVE_CHANGELIST_NEXT }
 	},
 	[VIS_ACTION_UNDO] = {
 		"vis-undo",

vis-core.h
@@ -153,19 +153,12 @@ struct File { /* shared state among windows displaying the same file */
 	File *next, *prev;
 };
 
-typedef struct {
-	time_t state;           /* state of the text, used to invalidate change list */
-	size_t index;           /* #number of changes */
-	size_t pos;             /* where the current change occured */
-} ChangeList;
-
 struct Win {
 	Vis *vis;               /* editor instance to which this window belongs to */
 	UiWin *ui;              /* ui object handling visual appearance of this window */
 	File *file;             /* file being displayed in this window */
 	View *view;             /* currently displayed part of underlying text */
 	MarkList jumplist;      /* LRU jump management */
-	ChangeList changelist;  /* state for iterating through least recently changes */
 	Mode modes[VIS_MODE_INVALID]; /* overlay mods used for per window key bindings */
 	Win *parent;            /* window which was active when showing the command prompt */
 	Mode *parent_mode;      /* mode which was active when showing the command prompt */

vis-motions.c
@@ -171,40 +171,6 @@ static size_t view_lines_bottom(Vis *vis, View *view) {
 	return view_screenline_goto(vis->win->view, h - vis_count_get_default(vis, 0));
 }
 
-static size_t window_changelist_next(Vis *vis, Win *win, size_t pos) {
-	ChangeList *cl = &win->changelist;
-	Text *txt = win->file->text;
-	time_t state = text_state(txt);
-	if (cl->state != state)
-		cl->index = 0;
-	else if (cl->index > 0 && pos == cl->pos)
-		cl->index--;
-	size_t newpos = pos;
-	if (newpos == EPOS)
-		cl->index++;
-	else
-		cl->pos = newpos;
-	cl->state = state;
-	return cl->pos;
-}
-
-static size_t window_changelist_prev(Vis *vis, Win *win, size_t pos) {
-	ChangeList *cl = &win->changelist;
-	Text *txt = win->file->text;
-	time_t state = text_state(txt);
-	if (cl->state != state)
-		cl->index = 0;
-	else if (pos == cl->pos)
-		win->changelist.index++;
-	size_t newpos = pos;
-	if (newpos == EPOS)
-		cl->index--;
-	else
-		cl->pos = newpos;
-	cl->state = state;
-	return cl->pos;
-}
-
 static size_t window_nop(Vis *vis, Win *win, size_t pos) {
 	return pos;
 }
@@ -585,14 +551,6 @@ const Movement vis_motions[] = {
 	[VIS_MOVE_WINDOW_LINE_BOTTOM] = {
 		.view = view_lines_bottom,
 		.type = LINEWISE|JUMP|IDEMPOTENT,
-	},
-	[VIS_MOVE_CHANGELIST_NEXT] = {
-		.win = window_changelist_next,
-		.type = INCLUSIVE,
-	},
-	[VIS_MOVE_CHANGELIST_PREV] = {
-		.win = window_changelist_prev,
-		.type = INCLUSIVE,
 	},
 	[VIS_MOVE_NOP] = {
 		.win = window_nop,