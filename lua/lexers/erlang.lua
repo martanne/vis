cb41e7e0c0209d3f1b04f57016ec674c2e26a4d5
Emil Falk <emph@riseup.net>
2017-11-20 23:29:50 +0100

    Update erlang syntax lexer.

--- lua/lexers/erlang.lua
+++ lua/lexers/erlang.lua
@@ -14,54 +14,34 @@ local ws = token(l.WHITESPACE, l.space^1)
 local comment = token(l.COMMENT, '%' * l.nonnewline^0)
 
 -- Strings.
-local sq_str = l.delimited_range("'", true)
-local dq_str = l.delimited_range('"')
-local literal = '$' * l.any * l.alnum^0
-local string = token(l.STRING, sq_str + dq_str + literal)
+local string = token(l.STRING, l.delimited_range('"'))
 
 -- Numbers.
-local number = token(l.NUMBER, l.float + l.integer)
+local const_char = '$' * (('\\' * l.ascii) + l.any)
+local number = token(l.NUMBER, const_char + l.float + l.integer)
+
+-- Atoms.
+local atom_pat = (l.lower * (l.alnum + '_')^0) + l.delimited_range("'")
+local atom = token(l.LABEL, atom_pat)
+
+-- Functions.
+local func = token(l.FUNCTION, atom_pat * #l.delimited_range("()", false, false, true))
 
 -- Keywords.
 local keyword = token(l.KEYWORD, word_match{
   'after', 'begin', 'case', 'catch', 'cond', 'end', 'fun', 'if', 'let', 'of',
-  'query', 'receive', 'when',
-  -- Operators.
-  'div', 'rem', 'or', 'xor', 'bor', 'bxor', 'bsl', 'bsr', 'and', 'band', 'not',
-  'bnot',
-  'badarg', 'nocookie', 'false', 'true'
-})
-
--- Functions.
-local func = token(l.FUNCTION, word_match{
-  'abs', 'alive', 'apply', 'atom_to_list', 'binary_to_list', 'binary_to_term',
-  'concat_binary', 'date', 'disconnect_node', 'element', 'erase', 'exit',
-  'float', 'float_to_list', 'get', 'get_keys', 'group_leader', 'halt', 'hd',
-  'integer_to_list', 'is_alive', 'length', 'link', 'list_to_atom',
-  'list_to_binary', 'list_to_float', 'list_to_integer', 'list_to_pid',
-  'list_to_tuple', 'load_module', 'make_ref', 'monitor_node', 'node', 'nodes',
-  'now', 'open_port', 'pid_to_list', 'process_flag', 'process_info', 'process',
-  'put', 'register', 'registered', 'round', 'self', 'setelement', 'size',
-  'spawn', 'spawn_link', 'split_binary', 'statistics', 'term_to_binary',
-  'throw', 'time', 'tl', 'trunc', 'tuple_to_list', 'unlink', 'unregister',
-  'whereis',
-  -- Others.
-  'atom', 'binary', 'constant', 'function', 'integer', 'list', 'number', 'pid',
-  'ports', 'port_close', 'port_info', 'reference', 'record',
-  -- Erlang:.
-  'check_process_code', 'delete_module', 'get_cookie', 'hash', 'math',
-  'module_loaded', 'preloaded', 'processes', 'purge_module', 'set_cookie',
-  'set_node',
-  -- Math.
-  'acos', 'asin', 'atan', 'atan2', 'cos', 'cosh', 'exp', 'log', 'log10', 'pi',
-  'pow', 'power', 'sin', 'sinh', 'sqrt', 'tan', 'tanh'
+  'query', 'receive', 'when'
 })
 
 -- Identifiers.
-local identifier = token(l.IDENTIFIER, l.word)
+local identifier = token(l.IDENTIFIER, ((l.upper + '_') * (l.alnum + '_')^0))
 
 -- Operators.
-local operator = token(l.OPERATOR, S('-<>.;=/|#+*:,?!()[]{}'))
+local named_operator = word_match{
+  'div', 'rem', 'or', 'xor', 'bor', 'bxor', 'bsl', 'bsr', 'and', 'band', 'not',
+  'bnot'
+}
+local operator = token(l.OPERATOR, S('-<>.;=/|#+*:,?!()[]{}') + named_operator)
 
 -- Directives.
 local directive = token('directive', '-' * word_match{
@@ -74,12 +54,13 @@ M._rules = {
   {'whitespace', ws},
   {'keyword', keyword},
   {'function', func},
+  {'operator', operator},
+  {'atom', atom},
   {'identifier', identifier},
   {'directive', directive},
   {'string', string},
   {'comment', comment},
-  {'number', number},
-  {'operator', operator},
+  {'number', number}
 }
 
 M._tokenstyles = {
