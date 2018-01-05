a94b52f7970ea5e6aa68fc5f5aa6c7de7884c7c7
Marc André Tanner <mat@brain-dump.org>
2018-01-05 16:16:41 +0100

    vis: remove now unused variable in prompt handling code

--- vis-prompt.c
+++ vis-prompt.c
@@ -64,7 +64,6 @@ static const char *prompt_enter(Vis *vis, const char *keys, const Arg *arg) {
 		else if (prompt->file == vis->search_file)
 			pattern = "^(/|\\?)";
 		if (pattern && regex && text_regex_compile(regex, pattern, REG_EXTENDED|REG_NEWLINE) == 0) {
-			char c;
 			size_t end = text_line_end(txt, pos);
 			size_t prev = text_search_backward(txt, end, regex);
 			if (prev > pos)
