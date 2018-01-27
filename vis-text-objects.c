67a8de28cdc9496d5655b1a9c1bf4794004ca9f0
TwoFinger <Two-Finger@users.noreply.github.com>
2018-01-27 18:44:29 +0200

    Fix a typo in identifiers

--- vis-text-objects.c
+++ vis-text-objects.c
@@ -141,12 +141,12 @@ const TextObject vis_textobjects[] = {
 		.txt = text_object_angle_bracket,
 		.type = TEXTOBJECT_DELIMITED_INNER,
 	},
-	[VIS_TEXTOBJECT_OUTER_PARANTHESE] = {
-		.txt = text_object_paranthese,
+	[VIS_TEXTOBJECT_OUTER_PARENTHESIS] = {
+		.txt = text_object_parenthesis,
 		.type = TEXTOBJECT_DELIMITED_OUTER,
 	},
-	[VIS_TEXTOBJECT_INNER_PARANTHESE] = {
-		.txt = text_object_paranthese,
+	[VIS_TEXTOBJECT_INNER_PARENTHESIS] = {
+		.txt = text_object_parenthesis,
 		.type = TEXTOBJECT_DELIMITED_INNER,
 	},
 	[VIS_TEXTOBJECT_OUTER_QUOTE] = {
