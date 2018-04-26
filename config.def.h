55ea139a5bee4d3dd503ccc255865fc1c6c3fabe
Marc André Tanner <mat@brain-dump.org>
2018-04-26 15:32:07 +0200

    vis: remove gP
    This only removes the user visible mapping, the underlying implementation
    is kept for now. It is used in insert mode for the implementation of <C-r>
    (register insertion).

--- config.def.h
+++ config.def.h
@@ -180,7 +180,6 @@ static const KeyBinding bindings_operators[] = {
 	{ "c",                  ACTION(OPERATOR_CHANGE)                     },
 	{ "d",                  ACTION(OPERATOR_DELETE)                     },
 	{ "g~",                 ALIAS(":|tr '[:lower:][:upper:]' '[:upper:][:lower:]'<Enter>") },
-	{ "gP",                 ACTION(PUT_BEFORE_END)                      },
 	{ "gu",                 ALIAS(":|tr '[:upper:]' '[:lower:]'<Enter>")},
 	{ "gU",                 ALIAS(":|tr '[:lower:]' '[:upper:]'<Enter>")},
 	{ "p",                  ACTION(PUT_AFTER)                           },
