668c49634d91d10809415720125a0033d3e31623
Javier Olaechea <pirata@gmail.com>
2018-05-16 21:48:23 -0500

    vis: make % motion match quotes and backticks as well
    Close #703
    Fix #670

--- vis-motions.c
+++ vis-motions.c
@@ -181,7 +181,7 @@ static size_t window_nop(Vis *vis, Win *win, size_t pos) {
 }
 
 static size_t bracket_match(Text *txt, size_t pos) {
-	size_t hit = text_bracket_match_symbol(txt, pos, "(){}[]<>");
+	size_t hit = text_bracket_match_symbol(txt, pos, "(){}[]<>'\"`");
 	if (hit != pos)
 		return hit;
 	char current;
@@ -196,6 +196,9 @@ static size_t bracket_match(Text *txt, size_t pos) {
 		case ']':
 		case '<':
 		case '>':
+		case '"':
+		case '\'':
+		case '`':
 			return it.pos;
 		}
 		text_iterator_byte_next(&it, NULL);
