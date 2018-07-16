91e74e92c3f5d6c7045125d374b0b32e7a219520
Jani Juhani Sinervo <jani@sinervo.fi>
2018-07-16 17:40:32 +0300

    Add ability to access `tabwidth` from Lua
    This commit adds the ability to access the current tabwidth from a Lua
    plugin. This can be used, for instance, to calculate the current column
    in terms of character cells, instead of Unicode codepoints. I.e. a TAB
    character could be counted as `tabwidth` character cells, instead of as
    one.

--- vis-lua.c
+++ vis-lua.c
@@ -1364,6 +1364,10 @@ static int pipe_func(lua_State *L) {
  * @see windows
  */
 /***
+ * Current tabwidth.
+ * @tfield int tabwidth
+ */
+/***
  * Currently active mode.
  * @tfield modes mode
  */
@@ -1385,6 +1389,11 @@ static int vis_index(lua_State *L) {
 				obj_ref_new(L, vis->win, VIS_LUA_TYPE_WINDOW);
 			else
 				lua_pushnil(L);
+			return 1;
+		}
+
+		if (strcmp(key, "tabwidth") == 0) {
+			lua_pushunsigned(L, vis->tabwidth);
 			return 1;
 		}
 
