2a455cacaa5e50b4fa978125a53a7b180f5f71cd
Moesasji <miyamoto.31b@gmail.com>
2020-12-20 17:47:15 +0000

    lexers/sml: import upstream revision d7c687a
    Merged vis fix for global function introduced by commit:
    4af2de4fb0483d388c7a6e3d43d8a5b8bf78042e

--- lua/lexers/sml.lua
+++ lua/lexers/sml.lua
@@ -1,44 +1,40 @@
--- Copyright 2017 Murray Calavera. See LICENSE.
+-- Copyright 2017-2020 Murray Calavera. See LICENSE.
 -- Standard ML LPeg lexer.
 
-local l = require('lexer')
-local token = l.token
+local lexer = require('lexer')
+local token = lexer.token
 
 local function mlword(words)
-  return l.word_match(words, "'")
+  return lexer.word_match(words, "'")
 end
 
-local ws = token(l.WHITESPACE, l.space^1)
+local ws = token(lexer.WHITESPACE, lexer.space^1)
 
 -- single line comments are valid in successor ml
-local cl = '(*)' * l.nonnewline^0
-local comment = token(l.COMMENT, cl + l.nested_pair('(*', '*)'))
+local line_comment = lexer.to_eol('(*)')
+local block_comment = lexer.range('(*', '*)', false, false, true)
+local comment = token(lexer.COMMENT, line_comment + block_comment)
 
-local string = token(l.STRING, lpeg.P('#')^-1 * l.delimited_range('"', true))
+local string = token(lexer.STRING, lpeg.P('#')^-1 * lexer.range('"', true))
 
 local function num(digit)
   return digit * (digit^0 * lpeg.P('_'))^0 * digit^1 + digit
 end
 
-local int = num(l.digit)
+local int = num(lexer.digit)
 local frac = lpeg.P('.') * int
 local minus = lpeg.P('~')^-1
 local exp = lpeg.S('eE') * minus * int
 local real = int * frac^-1 * exp + int * frac * exp^-1
-local hex = num(l.xdigit)
+local hex = num(lexer.xdigit)
 local bin = num(lpeg.S('01'))
 
-local number = token(l.NUMBER,
-  lpeg.P('0w') * int
-  + (lpeg.P('0wx') + lpeg.P('0xw')) * hex
-  + (lpeg.P('0wb') + lpeg.P('0bw')) * bin
-  + minus * lpeg.P('0x') * hex
-  + minus * lpeg.P('0b') * bin
-  + minus * real
-  + minus * int
-)
+local number = token(lexer.NUMBER, lpeg.P('0w') * int +
+  (lpeg.P('0wx') + lpeg.P('0xw')) * hex +
+  (lpeg.P('0wb') + lpeg.P('0bw')) * bin + minus * lpeg.P('0x') * hex +
+  minus * lpeg.P('0b') * bin + minus * real + minus * int)
 
-local keyword = token(l.KEYWORD, mlword{
+local keyword = token(lexer.KEYWORD, mlword{
   'abstype', 'and', 'andalso', 'as', 'case', 'do', 'datatype', 'else', 'end',
   'exception', 'fn', 'fun', 'handle', 'if', 'in', 'infix', 'infixr', 'let',
   'local', 'nonfix', 'of', 'op', 'orelse', 'raise', 'rec', 'then',
@@ -49,15 +45,16 @@ local keyword = token(l.KEYWORD, mlword{
 })
 
 -- includes valid symbols for identifiers
-local operator = token(l.OPERATOR, lpeg.S('!*/+-^:@=<>()[]{},;._|#%&$?~`\\'))
+local operator = token(lexer.OPERATOR,
+  lpeg.S('!*/+-^:@=<>()[]{},;._|#%&$?~`\\'))
 
-local type = token(l.TYPE, mlword{
+local type = token(lexer.TYPE, mlword{
   'int', 'real', 'word', 'bool', 'char', 'string', 'unit',
   'array', 'exn', 'list', 'option', 'order', 'ref', 'substring', 'vector'
 })
 
 -- `real`, `vector` and `substring` are a problem
-local func = token(l.FUNCTION, mlword{
+local func = token(lexer.FUNCTION, mlword{
   'app', 'before', 'ceil', 'chr', 'concat', 'exnMessage', 'exnName',
   'explode', 'floor', 'foldl', 'foldr', 'getOpt', 'hd', 'ignore',
   'implode', 'isSome', 'length', 'map', 'not', 'null', 'ord', 'print',
@@ -67,26 +64,23 @@ local func = token(l.FUNCTION, mlword{
 })
 
 -- non-symbolic identifiers only
-local id = (l.alnum + "'" + '_')^0
-local aid = l.alpha * id
+local id = (lexer.alnum + "'" + '_')^0
+local aid = lexer.alpha * id
 local longid = (aid * lpeg.P('.'))^0 * aid
-local identifier = token(l.IDENTIFIER, l.lower * id)
-local typevar = token(l.VARIABLE, "'" * id)
+local identifier = token(lexer.IDENTIFIER, lexer.lower * id)
+local typevar = token(lexer.VARIABLE, "'" * id)
 local c = mlword{'true', 'false', 'nil'}
-local const = token(l.CONSTANT, l.upper * id + c)
-local structure = token(l.CLASS, aid * lpeg.P('.'))
+local const = token(lexer.CONSTANT, lexer.upper * id + c)
+local structure = token(lexer.CLASS, aid * lpeg.P('.'))
 
-local open
-  = token(l.KEYWORD, mlword{'open', 'structure', 'functor'})
-  * ws * token(l.CLASS, longid)
+local open = token(lexer.KEYWORD, mlword{'open', 'structure', 'functor'}) * ws *
+  token(lexer.CLASS, longid)
 
-local struct_dec
-  = token(l.KEYWORD, lpeg.P('structure')) * ws
-  * token(l.CLASS, aid) * ws
-  * token(l.OPERATOR, lpeg.P('=')) * ws
+local struct_dec = token(lexer.KEYWORD, lpeg.P('structure')) * ws *
+  token(lexer.CLASS, aid) * ws * token(lexer.OPERATOR, lpeg.P('=')) * ws
 
-local struct_new = struct_dec * token(l.KEYWORD, lpeg.P('struct'))
-local struct_alias = struct_dec * token(l.CLASS, longid)
+local struct_new = struct_dec * token(lexer.KEYWORD, lpeg.P('struct'))
+local struct_alias = struct_dec * token(lexer.CLASS, longid)
 
 local M = {_NAME = 'sml'}
 
