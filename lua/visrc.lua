21c6a7a9ce5bb3c61c046dbda21862f8b11625f5
Moesasji <miyamoto.31b@gmail.com>
2020-12-23 14:17:14 +0000

    lua: silence unused function argument warning

--- lua/visrc.lua
+++ lua/visrc.lua
@@ -5,7 +5,9 @@ vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
 end)
 
+-- luacheck: push ignore 212
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- Your per window configuration options e.g.
 	-- vis:command('set number')
 end)
+-- luacheck: pop
