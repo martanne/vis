347a5bea96474bc440e28011767da80ea0a93a41
tynanbe <contact@tynan.be>
2021-02-12 14:10:13 -0600

    Add bit string support

--- lua/lexers/gleam.lua
+++ lua/lexers/gleam.lua
@@ -7,7 +7,7 @@ local l = require("lexer")
 local token, word_match = l.token, l.word_match
 local B, P, R, S = lpeg.B, lpeg.P, lpeg.R, lpeg.S
 
-local FN, KEY, OP = l.FUNCTION, l.KEYWORD, l.OPERATOR
+local KEY, OP = l.KEYWORD, l.OPERATOR
 local name = (l.lower + "_") * (l.lower + l.digit + "_")^0
 
 local M = {_NAME = "gleam"}
@@ -20,32 +20,31 @@ local typ_tok = token(l.TYPE, l.upper * l.alnum^0)
 
 -- Modules
 local mod_name = token("module", name)
-local mod_name_tok = mod_name * s^0 * token(OP, ".")
-local typ_or_fn = (typ_tok + token(FN, name))
+local fn_name = token(l.FUNCTION, name)
+local typ_or_fn = (typ_tok + fn_name)
 local mod_tok =
-  mod_name_tok
-  + token(KEY, "import") * s^1 * mod_name
-    * (s^0 * token(OP, "/") * s^0 * mod_name)^0
-    * (s^1 * token(KEY, "as") * s^1 * mod_name)^-1
-    * (
-      s^0 * token(OP, ".") * s^0 * token(OP, "{")
-      * s^0 * typ_or_fn
-      * (s^0 * token(OP, ",") * s^0 * typ_or_fn)^0
-      * s^0 * token(OP, "}")
-    )^-1
-
--- Functions
-local fn_tok =
-  token(FN, name * #(l.space^0 * P("(")))
-  + token(OP, "|>") * s^0
-    * (token(KEY, "fn") + mod_name_tok^-1 * token(FN, name))
+  token(KEY, "import") * s^1 * mod_name
+  * (s^0 * token(OP, "/") * s^0 * mod_name)^0
+  * (s^1 * token(KEY, "as") * s^1 * mod_name)^-1
+  * (
+    s^0 * token(OP, ".") * s^0 * token(OP, "{")
+    * s^0 * typ_or_fn
+    * (s^0 * token(OP, ",") * s^0 * typ_or_fn)^0
+    * s^0 * token(OP, "}")
+  )^-1
 
 -- Keywords
 local keyword = word_match{
-  "as", "assert", "case", "const", "external", "fn", "if", "import", "let", "opaque",
-  "pub", "todo", "try", "tuple", "type",
+  "as", "assert", "case", "const", "external", "fn", "if", "import", "let",
+  "opaque", "pub", "todo", "try", "tuple", "type",
 }
 local key_tok = token(KEY, keyword)
+
+-- Functions
+local mod_name_tok = mod_name * s^0 * token(OP, ".")
+local fn_tok =
+  mod_name_tok^-1 * s^0 * fn_name * s^0 * token(OP, "(")
+  + token(OP, "|>") * s^0 * (token(KEY, "fn") + mod_name_tok^-1 * fn_name)
 
 -- Identifiers
 local discard_name = P("_") * name
@@ -82,8 +81,8 @@ M._rules = {
   {"whitespace", s},
   {"type", typ_tok},
   {"module", mod_tok},
-  {"function", fn_tok},
   {"keyword", key_tok},
+  {"function", fn_tok},
   {"identifier", id_tok},
   {"string", str_tok},
   {"comment", com_tok},
@@ -102,30 +101,29 @@ M._foldsymbols = {
   [OP] = {["{"] = 1, ["}"] = -1, ["["] = 1, ["]"] = -1, ["("] = 1, [")"] = -1},
 }
 
--- Imports
---local import = {_NAME = "gleam_import"}
---import._rules = {
---  {"whitespace", s},
---  {"keyword", token(KEY, word_match{"as"})},
---  {"module", mod_name},
---  {"operator", op_tok},
---  {"error", err_tok},
---}
---local import_begin = token(KEY, P("import"))
---local import_end = token("import", #(keyword - P("as")))
---l.embed_lexer(M, import, import_begin, import_end)
-
--- Unqualified imports
---local unqual = {_NAME = "gleam_unqual"}
---unqual._rules = {
---  {"whitespace", s},
---  {"type", typ_tok},
---  {"identifier", token(FN, name)},
---  {"operator", op_tok},
---  {"error", err_tok},
---}
---local unqual_begin = token(OP, ".")
---local unqual_end = token(OP, "}")
---l.embed_lexer(import, unqual, unqual_begin, unqual_end)
+-- Bit Strings
+local bitstring = {_NAME = "gleam_bitstring"}
+local bstr_key_tok =
+  key_tok
+  + token(KEY, word_match{
+    "binary", "bytes", "int", "float", "bit_string", "bits",
+    "utf8", "utf16", "utf32", "utf8_codepoint", "utf16_codepoint", "utf32_codepoint",
+    "signed", "unsigned", "big", "little", "native", "unit", "size",
+  })
+bitstring._rules = {
+  {"whitespace", s},
+  {"type", typ_tok},
+  {"module", mod_tok},
+  {"keyword", bstr_key_tok},
+  {"function", fn_tok},
+  {"identifier", id_tok},
+  {"string", str_tok},
+  {"comment", com_tok},
+  {"number", num_tok},
+  {"operator", op_tok},
+}
+local bitstring_begin = token(OP, "<<")
+local bitstring_end = token(OP, ">>")
+l.embed_lexer(M, bitstring, bitstring_begin, bitstring_end)
 
 return M
