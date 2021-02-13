3f3e2d14e05ceb13730795ffe3f14fcbcd53168f
tynanbe <contact@tynan.be>
2021-02-12 23:26:20 -0600

    Add labels; refactor

--- lua/lexers/gleam.lua
+++ lua/lexers/gleam.lua
@@ -7,8 +7,12 @@ local l = require("lexer")
 local token, word_match = l.token, l.word_match
 local B, P, R, S = lpeg.B, lpeg.P, lpeg.R, lpeg.S
 
-local KEY, OP = l.KEYWORD, l.OPERATOR
+local ID, KEY, OP = l.IDENTIFIER, l.KEYWORD, l.OPERATOR
 local name = (l.lower + "_") * (l.lower + l.digit + "_")^0
+
+local fn_name = token(l.FUNCTION, name)
+local id = token(ID, name)
+local mod_name = token("module", name)
 
 local M = {_NAME = "gleam"}
 
@@ -19,8 +23,6 @@ local s = token(l.WHITESPACE, l.space^1)
 local typ_tok = token(l.TYPE, l.upper * l.alnum^0)
 
 -- Modules
-local mod_name = token("module", name)
-local fn_name = token(l.FUNCTION, name)
 local typ_or_fn = (typ_tok + fn_name)
 local mod_tok =
   token(KEY, "import") * s^1 * mod_name
@@ -40,39 +42,41 @@ local keyword = word_match{
 }
 local key_tok = token(KEY, keyword)
 
--- Functions
-local mod_name_tok = mod_name * s^0 * token(OP, ".")
-local fn_tok =
-  mod_name_tok^-1 * s^0 * fn_name * s^0 * token(OP, "(")
-  + token(OP, "|>") * s^0 * (token(KEY, "fn") + mod_name_tok^-1 * fn_name)
-
 -- Identifiers
-local discard_name = P("_") * name
-local id_tok = token("discard", discard_name) + token(l.IDENTIFIER, name)
+local discard_id = token("discard", "_" * name)
+local id_tok = discard_id + id
+
+-- Functions
+local mod_name_op = mod_name * s^0 * token(OP, ".")
+local fn_def_call = mod_name_op^-1 * s^0 * fn_name * s^0 * #P("(")
+local fn_pipe = token(OP, "|>") * s^0 * (token(KEY, "fn") + mod_name_op^-1 * fn_name)
+local fn_tok = fn_def_call + fn_pipe
 
 -- Strings
 local str_tok = token(l.STRING, l.delimited_range('"', false))
 
 -- Comments
-local line_comment = "//" * l.nonnewline^0
-local com_tok = token(l.COMMENT, line_comment + discard_name)
+local com_tok = token(l.COMMENT, "//" * l.nonnewline^0)
+
+-- Labels
+local lab_tok = token(OP, S("(,")) * s^0 * token(l.LABEL, name) * #(s^1 * id)
 
 -- Numbers
 local function can_neg(patt)
-  return (B(l.space + S("+-/*%<>=&|:,.")) * P("-"))^-1 * patt
+  return (B(l.space + S("+-/*%<>=&|:,.")) * "-")^-1 * patt
 end
 local function can_sep(patt)
-  return ((P("_")^-1 * patt^1) + patt)^1
+  return (P("_")^-1 * patt^1 + patt)^1
 end
 local dec = l.digit * can_sep(l.digit)^0
-local float = dec * P(".") * dec^0
-local bin = P("0") * S("bB") * can_sep(S("01"))
-local oct = P("0") * S("oO") * can_sep(R("07"))
-local hex = P("0") * S("xX") * can_sep(l.xdigit)
+local float = dec * "." * dec^0
+local bin = "0" * S("bB") * can_sep(S("01"))
+local oct = "0" * S("oO") * can_sep(R("07"))
+local hex = "0" * S("xX") * can_sep(l.xdigit)
 local num_tok = token(l.NUMBER, can_neg(float) + bin + oct + hex + can_neg(dec))
 
 -- Operators
-local op_tok = token(OP, S("+-/*%<>!=&|#:,.()[]{}"))
+local op_tok = token(OP, S("+-*/%#!=<>&|;:,.{}[]()"))
 
 -- Errors
 local err_tok = token(l.ERROR, l.any)
@@ -83,11 +87,13 @@ M._rules = {
   {"module", mod_tok},
   {"keyword", key_tok},
   {"function", fn_tok},
+  {"label", lab_tok},
   {"identifier", id_tok},
   {"string", str_tok},
   {"comment", com_tok},
   {"number", num_tok},
   {"operator", op_tok},
+  {"errors", err_tok},
 }
 
 M._tokenstyles = {
@@ -116,11 +122,13 @@ bitstring._rules = {
   {"module", mod_tok},
   {"keyword", bstr_key_tok},
   {"function", fn_tok},
+  {"label", lab_tok},
   {"identifier", id_tok},
   {"string", str_tok},
   {"comment", com_tok},
   {"number", num_tok},
   {"operator", op_tok},
+  {"errors", err_tok},
 }
 local bitstring_begin = token(OP, "<<")
 local bitstring_end = token(OP, ">>")
