martanne/vis

commit c22b2c2937d9dd65d0609e57f948f08da7f5c5ea

fix bug where visual-line selections after view were considered visible
prior to this patch, if you had a visual-line selection after the view,
and try to move it(& all other selections) up into the buffer, the
selection would appear prematurely.

https://github.com/martanne/vis/issues/1074

view.c
@@ -283,7 +283,7 @@ bool view_coord_get(View *view, size_t pos, Line **retline, int *retrow, int *re
 	size_t cur = view->start;
 	Line *line = view->topline;
 
-	if (pos < view->start || pos > view->end) {
+	if (pos < view->start || pos >= view->end) {
 		if (retline) *retline = NULL;
 		if (retrow) *retrow = -1;
 		if (retcol) *retcol = -1;

commit 364d21291a0a3848efeed615e5da69c757a5d2b0

check for EOF before unsetting row, col & line cache in view_coord_get
This commit fixes c22b2c2, which introduced a bug when the EOF was
in view.

view.c
@@ -283,7 +283,8 @@ bool view_coord_get(View *view, size_t pos, Line **retline, int *retrow, int *re
 	size_t cur = view->start;
 	Line *line = view->topline;
 
-	if (pos < view->start || pos >= view->end) {
+	bool eof = view->end == text_size(view->text);
+	if (pos < view->start || (pos >= view->end && !eof)) {
 		if (retline) *retline = NULL;
 		if (retrow) *retrow = -1;
 		if (retcol) *retcol = -1;