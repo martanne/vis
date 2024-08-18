57aacac06814dd5d72421b6e5cd969f96a7cd061
mitchell <70453897+orbitalquark@users.noreply.github.com>
2024-08-18 16:27:39 -0400

    Improve performance of the text lexer
    Instead of highlighting non-whitespace characters one at a time,
    highlight whole ranges.

--- lua/lexers/text.lua
+++ lua/lexers/text.lua
@@ -7,4 +7,6 @@ local lex = lexer.new('text')
 
 lex:add_rule('whitespace', lexer.token(lexer.WHITESPACE, lexer.space^1))
 
+lex:add_rule('text', lexer.token(lexer.DEFAULT, (1 - lexer.space)^1))
+
 return lex
