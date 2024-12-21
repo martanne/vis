0dfa1163620dc08c9853bfc47a455381b82828f1
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2024-12-21 13:10:44 -0500

    Migrate Haskell lexer
    Thanks to Samuel Marquis.

--- lua/lexers/haskell.lua
+++ lua/lexers/haskell.lua
@@ -1,47 +1,47 @@
 -- Copyright 2006-2024 Mitchell. See LICENSE.
 -- Haskell LPeg lexer.
 -- Modified by Alex Suraci.
+-- Migrated by Samuel Marquis.
 
-local lexer = require('lexer')
-local token, word_match = lexer.token, lexer.word_match
+local lexer = lexer
 local P, S = lpeg.P, lpeg.S
 
-local lex = lexer.new('haskell', {fold_by_indentation = true})
-
--- Whitespace.
-lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
+local lex = lexer.new(..., {fold_by_indentation = true})
 
 -- Keywords.
-lex:add_rule('keyword', token(lexer.KEYWORD, word_match{
-	'case', 'class', 'data', 'default', 'deriving', 'do', 'else', 'if', 'import', 'in', 'infix',
-	'infixl', 'infixr', 'instance', 'let', 'module', 'newtype', 'of', 'then', 'type', 'where', '_',
-	'as', 'qualified', 'hiding'
-}))
+lex:add_rule('keyword', lex:tag(lexer.KEYWORD, lex:word_match(lexer.KEYWORD)))
 
 -- Types & type constructors.
 local word = (lexer.alnum + S("._'#"))^0
 local op = lexer.punct - S('()[]{}')
-lex:add_rule('type', token(lexer.TYPE, (lexer.upper * word) + (':' * (op^1 - ':'))))
+lex:add_rule('type', lex:tag(lexer.TYPE, (lexer.upper * word) + (':' * (op^1 - ':'))))
 
 -- Identifiers.
-lex:add_rule('identifier', token(lexer.IDENTIFIER, (lexer.alpha + '_') * word))
+lex:add_rule('identifier', lex:tag(lexer.IDENTIFIER, (lexer.alpha + '_') * word))
 
 -- Strings.
 local sq_str = lexer.range("'", true)
 local dq_str = lexer.range('"')
-lex:add_rule('string', token(lexer.STRING, sq_str + dq_str))
+lex:add_rule('string', lex:tag(lexer.STRING, sq_str + dq_str))
 
 -- Comments.
 local line_comment = lexer.to_eol('--', true)
 local block_comment = lexer.range('{-', '-}')
-lex:add_rule('comment', token(lexer.COMMENT, line_comment + block_comment))
+lex:add_rule('comment', lex:tag(lexer.COMMENT, line_comment + block_comment))
 
 -- Numbers.
-lex:add_rule('number', token(lexer.NUMBER, lexer.number))
+lex:add_rule('number', lex:tag(lexer.NUMBER, lexer.number))
 
 -- Operators.
-lex:add_rule('operator', token(lexer.OPERATOR, op))
+lex:add_rule('operator', lex:tag(lexer.OPERATOR, op))
 
 lexer.property['scintillua.comment'] = '--'
+
+-- Word lists.
+lex:set_word_list(lexer.KEYWORD, {
+	'case', 'class', 'data', 'default', 'deriving', 'do', 'else', 'if', 'import', 'in', 'infix',
+	'infixl', 'infixr', 'instance', 'let', 'module', 'newtype', 'of', 'then', 'type', 'where', '_',
+	'as', 'qualified', 'hiding'
+})
 
 return lex
