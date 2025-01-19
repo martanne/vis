99683eba1c2700795d3fe782a1572e2ae1041205
infastin <infastin@yandex.com>
2025-01-20 02:22:57 +0500

    revert themes change

--- lua/themes/solarized.lua
+++ lua/themes/solarized.lua
@@ -92,6 +92,9 @@ lexers.STYLE_COMMAND_SECTION = lexers.STYLE_CLASS
 lexers.STYLE_ENVIRONMENT = lexers.STYLE_TYPE
 lexers.STYLE_ENVIRONMENT_MATH = lexers.STYLE_NUMBER
 
+-- Makefile                                                                                                                                                                                                                                                                 │
+lexers.STYLE_TARGET = ''
+
 -- Markdown
 lexers.STYLE_HR = ''
 for i = 1,6 do lexers['STYLE_HEADING_H'..i] = lexers.STYLE_HEADING end
