630693bfcf94ddbb9e1c394f4e77c4d3fdabe372
Matěj Cepl <mcepl@cepl.eu>
2025-06-13 18:45:29 +0200

    doc: source_suffix should be a map, not a list

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 
 # The master toctree document.
 master_doc = 'index'
