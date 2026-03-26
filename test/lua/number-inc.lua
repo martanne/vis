martanne/vis

commit da84fe700880eb988ea0819f9c823547287f28a9

number-inc-dec: fix wrong increment in a word
Only the number under or after the cursor should be changed.
However, if we unconditionally use the text object to detect where the
number to increment is, words like 1rc3 could result in the number
to the left of the curser to be changed.

This is fixed by only looking at the word under the cursor if the
cursor actually is on a number.

lua/plugins/number-inc-dec.lua
@@ -18,17 +18,29 @@ local change = function(delta)
 	for selection in win:selections_iterator() do
 		local pos = selection.pos
 		if not pos then goto continue end
-		local word = file:text_object_word(pos);
-		if not word then goto continue end
-		local data = file:content(word.start, 1024)
+		local data = file:content(pos, 1024)
 		if not data then goto continue end
 		local s, e = pattern:match(data)
 		if not s then goto continue end
+		if s == 1 then
+			-- detect if the cursor is in the middle of a number
+			local word = file:text_object_word(pos)
+			-- the cursor is in the middle of a word
+			if word.start < pos then
+				local word_prefix = pos-word.start
+				data = file:content(word.start, word_prefix)..data
+				local e_old = e + word_prefix - 1
+				pos = pos - word_prefix
+				while e_old > e do
+					s, e = pattern:match(data, e)
+				end
+			end
+		end
 		data = string.sub(data, s, e-1)
 		if #data == 0 then goto continue end
 		-- align start and end for fileindex
-		s = word.start + s - 1
-		e = word.start + e - 1
+		s = pos + s - 1
+		e = pos + e - 1
 		local base, format, padding = 10, 'd', 0
 		if lexer.oct_num:match(data) then
 			base = 8

test/lua/number-inc.in
@@ -1,0 +1,3 @@
+1.2rc3
+1xx19xx
+1foo22bar3

test/lua/number-inc.lua
@@ -1,0 +1,26 @@
+require("plugins/number-inc-dec")
+local win = vis.win
+
+describe("between numbers", function()
+	it("between numbers", function()
+		win.selection.pos = 4
+		vis:feedkeys("<C-a>")
+		assert.are.equal("1.2rc4", win.file.lines[1])
+	end)
+end)
+
+describe("in number", function()
+	it("in number", function()
+		win.selection.pos = 11
+		vis:feedkeys("<C-a>")
+		assert.are.equal("1xx20xx", win.file.lines[2])
+	end)
+end)
+
+describe("end of word/file", function()
+	it("end of word/file", function()
+		win.selection.pos = 24
+		vis:feedkeys("<C-a>")
+		assert.are.equal("1foo22bar4", win.file.lines[3])
+	end)
+end)

commit b4ebb77620018d4eca70ee544a61e2adfa0d74c4

number-inc-dec: fix not changing the whole number
Reset the end marker in order to rescan the word to find the
complete number relative to the word's start.

Fixes: da84fe700880eb988ea0819f9c823547287f28a9
Fixes: 13c577a1b3802562dc43235a5a4a8e5acdd331a9

lua/plugins/number-inc-dec.lua
@@ -29,8 +29,9 @@ local change = function(delta)
 			if word.start < pos then
 				local word_prefix = pos-word.start
 				data = file:content(word.start, word_prefix)..data
-				s = s + word_prefix
-				local e_old = e + word_prefix
+				s = s + word_prefix -- make the start relative to the word's start
+				local e_old = e + word_prefix -- remember the old end relative to the word' start
+				e = 1 -- reset the end mark to search the word until we find the complete number
 				pos = pos - word_prefix
 				while e_old > e do
 					s, e = pattern:match(data, e)

test/lua/number-inc.in
@@ -2,3 +2,4 @@
 1xx19xx
 1foo22bar3
 11y
+19

test/lua/number-inc.lua
@@ -10,31 +10,45 @@ describe("between numbers", function()
 end)
 
 describe("in number", function()
-	it("in number", function()
+	it("enclosed number", function()
 		win.selection.pos = 11
 		vis:feedkeys("<C-a>")
 		assert.are.equal("1xx20xx", win.file.lines[2])
 	end)
-end)
 
-describe("end of word/file", function()
-	it("end of word/file", function()
+	it("enclosed number", function()
+		win.selection.pos = 10
+		vis:feedkeys("<C-a>")
+		assert.are.equal("1xx21xx", win.file.lines[2])
+	end)
+
+	it("end of word", function()
 		win.selection.pos = 24
 		vis:feedkeys("<C-a>")
 		assert.are.equal("1foo22bar4", win.file.lines[3])
 	end)
-end)
 
-describe("position in number", function()
-	it("first digit", function()
+	it("first digit word start", function()
 		win.selection.pos = 26
 		vis:feedkeys("<C-a>")
 		assert.are.equal("12y", win.file.lines[4])
 	end)
 
-	it("last digit", function()
+	it("last digit word start", function()
 		win.selection.pos = 27
 		vis:feedkeys("<C-a>")
 		assert.are.equal("13y", win.file.lines[4])
+	end)
+
+	it("first digit single number", function()
+		win.selection.pos = 30
+		vis:feedkeys("<C-a>")
+		assert.are.equal("20", win.file.lines[5])
+	end)
+
+	it("last digit single number", function()
+		win.selection.pos = 31
+		vis:feedkeys("<C-x>")
+		assert.are.equal("19", win.file.lines[5])
 	end)
 end)