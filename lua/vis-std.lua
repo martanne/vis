012e36661cc5046c69600f3c7dc18cb8cd58ebf2
Jörg Bakker <jorbakk@openmultimedia.org>
2026-05-28 10:15:32 +0200

    lexer: fixed syntax highlighting breaks on longer files
    If lexer starts parsing at a file offset determined by the horizon variable
    and this offset happens to be within a string, then all non-strings are
    highlighted as strings and vice versa.
    
    This only occurs with strings that are delimited symmetrically, e.g.
    single and double quotes. It doesn't affect other syntactic delimiters that are
    non-symmetric, e.g. parentheses.
    
    With this patch, if the horizon variable is set to -1, the whole file buffer
    is run through the lexer. Performance is still reasonably good, as no syntax
    highlighting takes place outside of the viewport.
    
    Fixes issue #1253

--- lua/vis-std.lua
+++ lua/vis-std.lua
@@ -46,7 +46,7 @@ vis:option_register("horizon", "number", function(horizon)
 	if not vis.win then return false end
 	vis.win.horizon = horizon
 	return true
-end, "Number of bytes to consider for syntax highlighting")
+end, "Number of bytes to consider for syntax highlighting (set to -1 for all)")
 
 vis.events.subscribe(vis.events.WIN_HIGHLIGHT, function(win)
 	if not win.syntax or not vis.lexers.load then return end
@@ -59,7 +59,7 @@ vis.events.subscribe(vis.events.WIN_HIGHLIGHT, function(win)
 	local horizon_max = win.horizon or 32768
 	local horizon = viewport.start < horizon_max and viewport.start or horizon_max
 	local view_start = viewport.start
-	local lex_start = viewport.start - horizon
+	local lex_start = horizon == -1 and 0 or viewport.start - horizon
 	viewport.start = lex_start
 	local data = win.file:content(viewport)
 	local token_styles = lexer._TAGS
