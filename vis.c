martanne/vis

commit e9791c2c70c54daa708ef3a3081402ff55d54faf

core: cache file display name
The name used in the statusbar is the file name with the current
working directory removed. This was getting recomputed on every
statusbar update. This is very wasteful and requires a transition
into the kernel. Instead compute this only when necessary: on file
open, and when the user changes the working directory.

vis.c
@@ -40,7 +40,7 @@ static void file_free(Vis *vis, File *file) {
 	for (size_t i = 0; i < LENGTH(file->marks); i++)
 		da_release(file->marks + i);
 	text_free(file->text);
-	free((char*)file->name);
+	free(file->filepath.data);
 
 	if (file->prev)
 		file->prev->next = file->next;
@@ -65,41 +65,62 @@ static File *file_new_text(Vis *vis, Text *text) {
 	return file;
 }
 
-static File *file_new(Vis *vis, const char *name, bool internal) {
-	char *name_absolute = NULL;
+VIS_INTERNAL str8
+vis_current_directory(char *buffer, s64 buffer_length)
+{
+	str8 result = {0};
+	if (getcwd(buffer, buffer_length))
+		result = str8_from_c_str(buffer);
+	return result;
+}
+
+VIS_INTERNAL void
+vis_file_set_name_relative(File *file, str8 directory)
+{
+	str8 name = file->filepath;
+	if (str8_is_prefix(directory, name) && directory.length != name.length)
+		name = str8_skip(name, directory.length + 1);
+	file->name = name;
+}
+
+VIS_INTERNAL File *
+file_new(Vis *vis, const char *name, bool internal)
+{
 	bool cmp_names = 0;
 	struct stat new;
 
+	str8 filepath = {0};
 	if (name) {
-		if (!(name_absolute = absolute_path(name)))
-			return NULL;
+		filepath = vis_absolute_path(name);
+		if (filepath.length <= 0)
+			return 0;
 
-		if (stat(name_absolute, &new)) {
+		if (stat((char *)filepath.data, &new)) {
 			if (errno != ENOENT) {
-				free(name_absolute);
-				return NULL;
+				free(filepath.data);
+				return 0;
 			}
 			cmp_names = 1;
 		}
 
-		File *existing = NULL;
+		File *existing = 0;
 		/* try to detect whether the same file is already open in another window */
 		for (File *file = vis->files; file; file = file->next) {
-			if (file->name) {
-				if ((cmp_names && strcmp(file->name, name_absolute) == 0) ||
-				    (file->stat.st_dev == new.st_dev && file->stat.st_ino == new.st_ino)) {
-					existing = file;
-					break;
-				}
+			if ((cmp_names && str8_equal(file->filepath, filepath)) ||
+			    (file->stat.st_dev == new.st_dev && file->stat.st_ino == new.st_ino))
+			{
+				existing = file;
+				break;
 			}
 		}
+
 		if (existing) {
-			free(name_absolute);
+			free(filepath.data);
 			return existing;
 		}
 	}
 
-	File *file = NULL;
+	File *file = 0;
 	Text *text = text_load_method(vis, name, vis->load_method);
 	if (!text && name && errno == ENOENT)
 		text = text_load(vis, 0);
@@ -107,16 +128,20 @@ static File *file_new(Vis *vis, const char *name, bool internal) {
 		goto err;
 	if (!(file = file_new_text(vis, text)))
 		goto err;
-	file->name = name_absolute;
+	file->filepath = filepath;
 	file->internal = internal;
 	if (!internal)
 		vis_event_emit(vis, VIS_EVENT_FILE_OPEN, file);
+
+	char cwd_buffer[PATH_MAX];
+	vis_file_set_name_relative(file, vis_current_directory(cwd_buffer, countof(cwd_buffer)));
+
 	return file;
 err:
-	free(name_absolute);
+	free(filepath.data);
 	text_free(text);
 	file_free(vis, file);
-	return NULL;
+	return 0;
 }
 
 static File *file_new_internal(Vis *vis, const char *filename) {
@@ -124,27 +149,6 @@ static File *file_new_internal(Vis *vis, const char *filename) {
 	if (file)
 		file->refcount = 1;
 	return file;
-}
-
-void file_name_set(File *file, const char *name) {
-	if (name == file->name)
-		return;
-	free((char*)file->name);
-	file->name = absolute_path(name);
-}
-
-const char *file_name_get(File *file) {
-	/* TODO: calculate path relative to working directory, cache result */
-	if (!file->name)
-		return NULL;
-	char cwd[PATH_MAX];
-	if (!getcwd(cwd, sizeof cwd))
-		return file->name;
-	const char *path = strstr(file->name, cwd);
-	if (path != file->name)
-		return file->name;
-	size_t cwdlen = strlen(cwd);
-	return file->name[cwdlen] == '/' ? file->name+cwdlen+1 : file->name;
 }
 
 void window_selection_save(Win *win)
@@ -374,14 +378,15 @@ Win *window_new_file(Vis *vis, File *file, enum UiOption options) {
 	return win;
 }
 
+// TODO(rnp): hack/refactor: vis_window_file_reload, add file_new force parameter
 bool vis_window_reload(Win *win) {
-	const char *name = win->file->name;
-	if (!name)
+	str8 path = win->file->filepath;
+	if (path.length <= 0)
 		return false; /* can't reload unsaved file */
 	/* temporarily unset file name, otherwise file_new returns the same File */
-	win->file->name = NULL;
-	File *file = file_new(win->vis, name, false);
-	win->file->name = name;
+	win->file->filepath = (str8){0};
+	File *file = file_new(win->vis, (char *)path.data, false);
+	win->file->filepath = path;
 	if (!file)
 		return false;
 	file_free(win->vis, win->file);
@@ -1274,7 +1279,7 @@ int vis_run(Vis *vis) {
 				next = win->next;
 				if (win->file->truncated) {
 					free(name);
-					name = strdup(win->file->name);
+					name = strndup((char *)win->file->filepath.data, win->file->filepath.length);
 					vis_window_close(win);
 				}
 			}
@@ -1660,10 +1665,11 @@ static int _vis_pipe(Vis *vis, File *file, Filerange range, const char* buf, con
 		close(perr[1]);
 		close(null);
 
-		if (file != NULL && file->name) {
-			char *name = strrchr(file->name, '/');
-			setenv("vis_filepath", file->name, 1);
-			setenv("vis_filename", name ? name+1 : file->name, 1);
+		if (file) {
+			str8 name;
+			path_split(file->filepath, 0, &name);
+			setenv("vis_filepath", file->filepath.data ? (char *)file->filepath.data : "", 1);
+			setenv("vis_filename", name.length > 0     ? (char *)name.data           : "", 1);
 		}
 
 		if (!argv[1])
@@ -1886,4 +1892,33 @@ Text *vis_text(Vis *vis) {
 View *vis_view(Vis *vis) {
 	Win *win = vis->win;
 	return win ? &win->view : NULL;
+}
+
+VIS_EXPORT bool
+vis_change_directory(Vis *vis, u8 *directory_name, s64 length)
+{
+	str8 directory = {.data = directory_name, .length = length};
+	u8 path[PATH_MAX];
+
+	if (length <= 0) {
+		// NOTE(rnp): guaranteed 0 terminated
+		directory = str8_from_c_str(getenv("HOME"));
+	} else if (length < countof(path) - 1) {
+		memcpy(path, directory_name, length);
+		directory.data = path;
+		path[length] = 0;
+	} else {
+		directory.length = 0;
+	}
+
+	bool result = directory.length > 0 && chdir((char *)directory.data) == 0;
+	if (result) {
+		// NOTE(rnp): resolve relative components
+		str8 wd = vis_current_directory((char *)path, countof(path));
+		for (File *file = vis->files; file; file = file->next)
+			if (!file->internal)
+				vis_file_set_name_relative(file, wd);
+	}
+
+	return result;
 }

sam.c
@@ -1425,7 +1425,7 @@ static bool cmd_files(Vis *vis, Win *win, Command *cmd, const char *argv[], Sele
 		if (w->file->internal)
 			continue;
 		bool match = !cmd->regex ||
-		             (w->file->name && text_regex_match(cmd->regex, w->file->name, 0) == 0);
+		             (w->file->filepath.length > 0 && text_regex_match(cmd->regex, (char *)w->file->filepath.data, 0) == 0);
 		if (match ^ (argv[0][0] == 'Y')) {
 			Filerange def = text_range_new(0, 0);
 			ret &= sam_execute(vis, w, cmd->cmd, NULL, &def);
@@ -1457,7 +1457,7 @@ static bool cmd_write(Vis *vis, Win *win, Command *cmd, const char *argv[], Sele
 
 	const char *filename = argv[1];
 	if (!filename)
-		filename = file->name;
+		filename = (char *)file->filepath.data;
 	if (!filename) {
 		if (file->fd == -1) {
 			vis_info_show(vis, "Filename expected");
@@ -1508,15 +1508,15 @@ static bool cmd_write(Vis *vis, Win *win, Command *cmd, const char *argv[], Sele
 
 	for (const char **name = argv[1] ? &argv[1] : (const char*[]){ filename, NULL }; *name; name++) {
 
-		char *path = absolute_path(*name);
-		if (!path) {
+		str8 path = vis_absolute_path(*name);
+		if (path.length <= 0) {
 			vis_info_show(vis, "Failed to resolve path '%s': %s", *name, strerror(errno));
 			return false;
 		}
 
 		struct stat meta;
-		bool existing_file = !stat(path, &meta);
-		bool same_file = existing_file && file->name &&
+		bool existing_file = !stat((char *)path.data, &meta);
+		bool same_file = existing_file && file->filepath.data &&
 		                 file->stat.st_dev == meta.st_dev && file->stat.st_ino == meta.st_ino;
 
 		if (cmd->flags != '!') {
@@ -1540,18 +1540,18 @@ static bool cmd_write(Vis *vis, Win *win, Command *cmd, const char *argv[], Sele
 			}
 		}
 
-		if (!vis_event_emit(vis, VIS_EVENT_FILE_SAVE_PRE, file, path) && cmd->flags != '!') {
-			vis_info_show(vis, "Rejected write to `%s' by pre-save hook", path);
+		if (!vis_event_emit(vis, VIS_EVENT_FILE_SAVE_PRE, file, path.data) && cmd->flags != '!') {
+			vis_info_show(vis, "Rejected write to `%.*s' by pre-save hook", (int)path.length, path.data);
 			goto err;
 		}
 		/* a pre-save hook may have changed the text; need to re-take the range */
 		if (write_entire_file)
 			*r = text_range_new(0, text_size(text));
 
-		TextSave ctx = text_save_default(.txt = text, .method = file->save_method, .filename = path);
+		TextSave ctx = text_save_default(.txt = text, .method = file->save_method, .filepath = path);
 		if (!text_save_begin(&ctx)) {
 			const char *msg = errno ? strerror(errno) : "try changing `:set savemethod`";
-			vis_info_show(vis, "Can't write `%s': %s", path, msg);
+			vis_info_show(vis, "Can't write `%.*s': %s", (int)path.length, path.data, msg);
 			goto err;
 		}
 
@@ -1573,22 +1573,26 @@ static bool cmd_write(Vis *vis, Win *win, Command *cmd, const char *argv[], Sele
 		}
 
 		if (failure) {
-			vis_info_show(vis, "Can't write `%s': %s", path, strerror(errno));
+			vis_info_show(vis, "Can't write `%.*s': %s", (int)path.length, path.data, strerror(errno));
 			goto err;
 		}
 
-		if (!file->name) {
-			file_name_set(file, path);
+		if (file->filepath.length == 0) {
+			file->filepath = path;
 			same_file = true;
 		}
-		if (same_file || (!existing_file && strcmp(file->name, path) == 0))
+
+		if (same_file || (!existing_file && str8_equal(file->filepath, path)))
 			file->stat = text_stat(text);
-		vis_event_emit(vis, VIS_EVENT_FILE_SAVE_POST, file, path);
-		free(path);
+
+		vis_event_emit(vis, VIS_EVENT_FILE_SAVE_POST, file, path.data);
+
+		if (file->filepath.data != path.data)
+			free(path.data);
 		continue;
 
 	err:
-		free(path);
+		free(path.data);
 		return false;
 	}
 	return true;
@@ -1653,11 +1657,13 @@ static bool cmd_pipeout(Vis *vis, Win *win, Command *cmd, const char *argv[], Se
 	return !vis->interrupted && status == 0;
 }
 
-static bool cmd_cd(Vis *vis, Win *win, Command *cmd, const char *argv[], Selection *sel, Filerange *range) {
-	const char *dir = argv[1];
-	if (!dir)
-		dir = getenv("HOME");
-	return dir && chdir(dir) == 0;
+static bool
+cmd_cd(Vis *vis, Win *win, Command *cmd, const char *argv[], Selection *sel, Filerange *range)
+{
+	// TODO(rnp): argv shouldn't need to be be 0 terminated copies of cmdline
+	str8 dir = str8_from_c_str(argv[1]);
+	bool result = vis_change_directory(vis, dir.data, dir.length);
+	return result;
 }
 
 #include "vis-cmds.c"

test/core/text-test.c
@@ -88,7 +88,7 @@ static void iterator_find_prev(Text *txt, size_t start, char b, size_t match) {
 }
 
 static bool text_save_method(Text *txt, const char *filename, enum TextSaveMethod method) {
-	TextSave ctx = text_save_default(.txt = txt, .filename = filename, .method = method);
+	TextSave ctx = text_save_default(.txt = txt, .filepath = str8_from_c_str(filename), .method = method);
 	if (!text_save_begin(&ctx))
 		return false;
 	Filerange range = (Filerange){ .start = 0, .end = text_size(txt) };

text-io.c
@@ -272,10 +272,10 @@ err:
 static bool text_save_begin_atomic(TextSave *ctx)
 {
 	int oldfd, saved_errno;
-	if ((oldfd = openat(ctx->dirfd, ctx->filename, O_RDONLY)) == -1 && errno != ENOENT)
+	if ((oldfd = openat(ctx->dirfd, (char *)ctx->filepath.data, O_RDONLY)) == -1 && errno != ENOENT)
 		goto err;
 	struct stat oldmeta = { 0 };
-	if (oldfd != -1 && fstatat(ctx->dirfd, ctx->filename, &oldmeta, AT_SYMLINK_NOFOLLOW) == -1)
+	if (oldfd != -1 && fstatat(ctx->dirfd, (char *)ctx->filepath.data, &oldmeta, AT_SYMLINK_NOFOLLOW) == -1)
 		goto err;
 	if (oldfd != -1) {
 		if (S_ISLNK(oldmeta.st_mode)) /* symbolic link */
@@ -284,7 +284,7 @@ static bool text_save_begin_atomic(TextSave *ctx)
 			goto err;
 	}
 
-	str8 base, dir, fname = str8_from_c_str((char *)ctx->filename);
+	str8 base, dir, fname = ctx->filepath;
 	path_split(fname, &dir, &base);
 
 	char suffix[] = ".vis.XXXXXX";
@@ -341,7 +341,7 @@ static bool text_save_commit_atomic(TextSave *ctx) {
 	if (close_failed)
 		return false;
 
-	if (renameat(ctx->dirfd, (char *)ctx->tmpname.data, ctx->dirfd, ctx->filename) == -1)
+	if (renameat(ctx->dirfd, (char *)ctx->tmpname.data, ctx->dirfd, (char *)ctx->filepath.data) == -1)
 		return false;
 
 
@@ -375,7 +375,7 @@ static bool text_save_begin_inplace(TextSave *ctx) {
 	Text *txt = ctx->txt;
 	struct stat now = { 0 };
 	int newfd = -1, saved_errno;
-	if ((ctx->fd = openat(ctx->dirfd, ctx->filename, O_CREAT|O_WRONLY, 0666)) == -1)
+	if ((ctx->fd = openat(ctx->dirfd, (char *)ctx->filepath.data, O_CREAT|O_WRONLY, 0666)) == -1)
 		goto err;
 	if (fstat(ctx->fd, &now) == -1)
 		goto err;

text.h
@@ -348,12 +348,11 @@ enum TextSaveMethod {
 typedef struct {
 	enum TextSaveMethod method; /* method used to save file */
 	Text *txt;                 /* text to operate on */
-	const char *filename;      /* filename to save to */
+	str8 filepath;             /* filepath to save to (0 terminated) */
 	str8 tmpname;              /* temporary name used for atomic rename(2) */
 	int fd;                    /* file descriptor to write data to using text_save_write */
 	int dirfd;                 /* directory file descriptor, relative to which we save */
 } TextSave;
-
 #define text_save_default(...) (TextSave){.dirfd = AT_FDCWD, .fd = -1, __VA_ARGS__}
 
 /**

util.c
@@ -126,6 +126,14 @@ str8_case_ignore_equal(str8 a, str8 b)
 	return result;
 }
 
+static bool
+str8_is_prefix(str8 prefix, str8 s)
+{
+	bool result = s.length >= prefix.length &&
+	              str8_equal(prefix, (str8){.data = s.data, .length = prefix.length});
+	return result;
+}
+
 static str8
 str8_skip(str8 s, int64_t count)
 {
@@ -141,7 +149,7 @@ static str8
 str8_skip_space(str8 s)
 {
 	str8 result = s;
-	for (ptrdiff_t i = 0; i < s.length && IsSpace(*result.data); result.data++);
+	for (s64 i = 0; i < s.length && IsSpace(*result.data); result.data++, i++);
 	result.length -= result.data - s.data;
 	return result;
 }
@@ -192,11 +200,13 @@ path_split(str8 path, str8 *directory, str8 *basename)
 	if (directory) *directory = left.length == 0 ? str8(".") : left;
 }
 
-static char *
-absolute_path(const char *name)
+// NOTE(rnp): returns 0 terminated str8
+VIS_INTERNAL str8
+vis_absolute_path(const char *name)
 {
+	str8 result = {0};
 	if (!name)
-		return 0;
+		return result;
 
 	str8 base, dir, string = str8_from_c_str((char *)name);
 	path_split(string, &dir, &base);
@@ -207,7 +217,6 @@ absolute_path(const char *name)
 	memcpy(path_normalized, dir.data, dir.length);
 	path_normalized[dir.length] = 0;
 
-	char *result = 0;
 	if (realpath(path_normalized, path_resolved) == path_resolved) {
 		str8 resolved = str8_from_c_str(path_resolved);
 		if (str8_equal(resolved, str8("/"))) {
@@ -216,12 +225,13 @@ absolute_path(const char *name)
 		}
 
 		int64_t total_length = base.length + resolved.length + 1;
-		result = malloc(total_length + 1);
-		if (result) {
-			memcpy(result, resolved.data, resolved.length);
-			memcpy(result + resolved.length + 1, base.data, base.length);
-			result[resolved.length] = '/';
-			result[total_length]    = 0;
+		result.data = malloc(total_length + 1);
+		if (result.data) {
+			memcpy(result.data, resolved.data, resolved.length);
+			memcpy(result.data + resolved.length + 1, base.data, base.length);
+			result.data[resolved.length] = '/';
+			result.data[total_length]    = 0;
+			result.length = total_length;
 		}
 	}
 	return result;

util.h
@@ -124,7 +124,19 @@ static inline bool addu(size_t a, size_t b, size_t *c) {
 #define PATH_MAX 4096
 #endif
 
-typedef int32_t VisDACount;
+
+typedef size_t    uz;
+typedef int64_t   s64;
+typedef uint64_t  u64;
+typedef int32_t   s32;
+typedef uint32_t  u32;
+typedef int16_t   s16;
+typedef uint16_t  u16;
+typedef int8_t    s8;
+typedef uint8_t   u8;
+typedef char      c8;
+
+#define VisDACount s32
 
 typedef struct {
 	ptrdiff_t  length;

view.c
@@ -67,14 +67,14 @@ void window_status_update(Vis *vis, Win *win) {
 	int width = win->width;
 	enum UiOption options = win->options;
 	bool focused = vis->win == win;
-	const char *filename = file_name_get(file);
+	str8 filename = file->name;
 	const char *mode = vis->mode->status;
 
 	if (focused && mode)
 		strcpy(left_parts[left_count++], mode);
 
 	snprintf(left_parts[left_count++], sizeof(left_parts[0]), "%s%s%s",
-	         filename ? filename : "[No Name]",
+	         filename.length > 0 ? (char *)filename.data : "[No Name]",
 	         text_modified(txt) ? " [+]" : "",
 	         vis_macro_recording(vis) ? " @": "");
 

commit 84ee9d1f3cf28c2a234db71fb721e6f7816fcba3

vis: file_new: don't look at internal files when matching
It was reported that when compiled in debug mode vis would show
"[No Name]" for all non existent files even when a name was
provided. Interestingly I could only get this to reproduce when
compiling with gcc. I'm not entirely sure why this error only
occurs in that configuration however I did notice that those files
were matching the internal files vis uses for the message window
and prompt. The logic for skipping these previously was an obtuse
check for if the File had a name so I mistakenly removed it. Now
the logic reads as it should and the issue is resolved.

fixes: e9791c2c

vis.c
@@ -106,8 +106,8 @@ file_new(Vis *vis, const char *name, bool internal)
 		File *existing = 0;
 		/* try to detect whether the same file is already open in another window */
 		for (File *file = vis->files; file; file = file->next) {
-			if ((cmp_names && str8_equal(file->filepath, filepath)) ||
-			    (file->stat.st_dev == new.st_dev && file->stat.st_ino == new.st_ino))
+			if (!file->internal && ((cmp_names && str8_equal(file->filepath, filepath)) ||
+			    (file->stat.st_dev == new.st_dev && file->stat.st_ino == new.st_ino)))
 			{
 				existing = file;
 				break;
@@ -1903,7 +1903,7 @@ vis_change_directory(Vis *vis, u8 *directory_name, s64 length)
 	if (length <= 0) {
 		// NOTE(rnp): guaranteed 0 terminated
 		directory = str8_from_c_str(getenv("HOME"));
-	} else if (length < countof(path) - 1) {
+	} else if (length < (s64)countof(path) - 1) {
 		memcpy(path, directory_name, length);
 		directory.data = path;
 		path[length] = 0;